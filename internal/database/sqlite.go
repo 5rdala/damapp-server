851e9e33cc2303ae0927f3f4695e346fefe32a6a
Marwane Souiri <marwane.souiri.0@gmail.com>
2025-04-07 01:51:28 +0100

    feat(database): implement SQLite database initialization and migration

---
+++ internal/database/sqlite.go
@@ -0,0 +1,27 @@
+package database
+
+import (
+	"damapp-server/internal/domain"
+
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+
+	"fmt"
+)
+
+func InitDB() (*gorm.DB, error) {
+	db, err := gorm.Open(sqlite.Open("./database.db"), &gorm.Config{})
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect to the database: %v", err)
+	}
+
+	if err := db.AutoMigrate(&domain.User{}); err != nil {
+		return nil, fmt.Errorf("failed to migrate database: %v", err)
+	}
+
+	if err := db.AutoMigrate(&domain.Friendship{}); err != nil {
+		return nil, fmt.Errorf("failed to migrate database: %v", err)
+	}
+
+	return db, nil
+}
