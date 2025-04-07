179e0b309974a0d30d2e6bb45011ec0f1126a460
Marwane Souiri <marwane.souiri.0@gmail.com>
2025-04-07 17:31:25 +0100

    feat(utils): implement Snowflake IDs generator

---
+++ utils/snowflake.go
@@ -0,0 +1,72 @@
+package utils
+
+import (
+	"sync"
+	"sync/atomic"
+	"time"
+)
+
+type snowflake struct {
+	epoch     int64
+	nodeID    uint64
+	lastTsSeq atomic.Uint64
+}
+
+const (
+	nodeBits     = 10
+	sequenceBits = 12
+	maxSequence  = (1 << sequenceBits) - 1
+
+	timeShift = nodeBits + sequenceBits
+	nodeShift = sequenceBits
+)
+
+var (
+	sfInstance *snowflake
+	once       sync.Once
+)
+
+func NewSnowFlake(epoch int64, nodeID uint64) *snowflake {
+	once.Do(func() {
+		sfInstance = &snowflake{
+			epoch:  epoch,
+			nodeID: nodeID,
+		}
+	})
+	return sfInstance
+}
+
+func GenerateID() uint64 {
+	if sfInstance == nil {
+		NewSnowFlake(time.Date(1970, 1, 1, 0, 0, 0, 0, time.UTC).UnixMilli(), 1)
+	}
+	return sfInstance.Generate()
+}
+
+func (s *snowflake) Generate() uint64 {
+	for {
+		now := uint64(time.Now().UnixMilli() - s.epoch)
+		packed := s.lastTsSeq.Load()
+		lastTs := packed >> sequenceBits
+		seq := packed & maxSequence
+
+		if now == lastTs {
+			if seq >= maxSequence {
+				for now <= lastTs {
+					now = uint64(time.Now().UnixMilli() - s.epoch)
+				}
+				seq = 0
+			} else {
+				seq++
+			}
+		} else {
+			seq = 0
+		}
+
+		newPacked := (now << sequenceBits) | seq
+		if s.lastTsSeq.CompareAndSwap(packed, newPacked) {
+			id := (now << timeShift) | (s.nodeID << nodeShift) | seq
+			return id
+		}
+	}
+}
