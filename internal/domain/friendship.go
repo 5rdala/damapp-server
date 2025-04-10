3437ba1459d32cd48601ac80e092aae20e33b183
Marwane Souiri <marwane.souiri.0@gmail.com>
2025-04-10 17:28:26 +0100

    feat(domain): add String method to FriendshipStatus enum

--- internal/domain/friendship.go
+++ internal/domain/friendship.go
@@ -8,6 +8,19 @@ const (
 	FriendshipStatusAccepted
 )
 
+func (s FriendshipStatus) String() string {
+	switch s {
+	case FriendshipStatusPending:
+		return "pending"
+	case FriendshipStatusRejected:
+		return "rejected"
+	case FriendshipStatusAccepted:
+		return "accepted"
+	default:
+		return "unknown"
+	}
+}
+
 // UserID1 is the sender of the Friendship request
 // UserID2 is the recipient of the Friendship request
 type Friendship struct {
